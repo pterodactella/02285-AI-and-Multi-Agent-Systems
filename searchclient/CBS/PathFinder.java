a577044f5c062503976ca3715bb20df1e98d3dca
Daniela <daniela.bahneanu@gmail.com>
2023-05-06 14:38:41 +0200

    removed some comments

--- searchclient/CBS/PathFinder.java
+++ searchclient/CBS/PathFinder.java
@@ -18,8 +18,6 @@ public class PathFinder {
 		root.solution = root.findPlan();
 		root.cost = root.sumCosts();
 
-		//TODO: Replace with priority queue (Done)
-		// Stack<CBSNode> open = new Stack<>();
 		PriorityQueue<CBSNode> open = new PriorityQueue<>(new Comparator<CBSNode>() {
 			@Override
 			public int compare(CBSNode n1, CBSNode n2) {
@@ -41,20 +39,16 @@ public class PathFinder {
 			 for (int agentIndex : c.agentIndexes) {
 				 CBSNode a = new CBSNode(this.initialState);
 				 a.constraints.add(new Constraint(agentIndex,c.locationX,c.locationY,c.timestamp));
-				 //a.solution = p.solution
+
 				 a.solution = new Action[p.solution.length][];
 				 for(int i = 0; i < p.solution.length; i++)
 					    a.solution[i] = p.solution[i].clone();
 						
-				//TODO: Recalculate only for one(agentIndex) (Done)
-				//  a.solution = a.findPlan();
-				//  a.cost = a.sumCosts();
 
 				Action[][] individualPlans = a.findPlan();
 				a.solution[agentIndex] = individualPlans[agentIndex];
 				a.cost = a.sumCosts();
 				 
-				 //TODO: use a number instead of infinity (Done) 
 				 a.cost = Integer.MAX_VALUE;
 				 open.add(a);
 			 }
