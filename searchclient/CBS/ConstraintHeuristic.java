7ff00ec8776bc90d6a41f3b24abed7711911ab73
Odelia <deelyohuang@gmail.com>
2023-05-15 19:32:06 +0200

    Revert "merged refactor into main"
    This reverts commit 5a346820df3ab47c665cfc221ec2578aba7aeabd.

--- searchclient/CBS/ConstraintHeuristic.java
+++ searchclient/CBS/ConstraintHeuristic.java
@@ -1,5 +1,3 @@
-
-
 package searchclient.CBS;
 
 import java.util.Comparator;
@@ -9,152 +7,101 @@ import searchclient.Distances;
 import searchclient.ManhattanDistance;
 
 public abstract class ConstraintHeuristic implements Comparator<ConstraintState> {
-    public ConstraintHeuristic() {
-        // Here's a chance to pre-process the static parts of the level.
-    }
-	public int h(ConstraintState s)
-    {
-    	Distances d = new ManhattanDistance(s.agentRows, s.agentCols, s.goals, s.boxes);
-   	// System.out.println("d=" );
-   	// System.out.println(d.calculate());
-        return d.calculate();
-    }
+	public ConstraintHeuristic() {
+// Here's a chance to pre-process the static parts of the level.
+	}
 
 
-    public int calculateConstraintPenalty(ConstraintState s) {
-        // Calculate the penalty based on the number of constraints in the state
-        // You can define your own function to determine the penalty value
+	public int h(ConstraintState s) {
+		// Compute the heuristic for the specific goal state using Manhattan distance or
+		// any other suitable method
+		// Implement your own heuristic calculation here
 
-        int numConstraints = s.constraints.size();
-        int penalty = numConstraints * 10; // Adjust the penalty factor as needed
+		Distances d = new ManhattanDistance(s.agentRows, s.agentCols, s.goals, s.boxes);
+		int heuristic = d.calculate();
 
-        return penalty;
-    }
+		return heuristic;
+	}
 
-    public abstract int f(ConstraintState s);
+	public abstract int f(ConstraintState s);
 
-    @Override
-    public int compare(ConstraintState s1, ConstraintState s2) {
-        int f1 = this.f(s1);
-        int f2 = this.f(s2);
+	@Override
+	public int compare(ConstraintState s1, ConstraintState s2) {
+		int f1 = this.f(s1);
+		int f2 = this.f(s2);
 
-        if (f1 < f2) {
-            return -1;
-        } else if (f1 > f2) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
+		if (f1 < f2) {
+			return -1;
+		} else if (f1 > f2) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
 
 }
 
 class ConstraintHeuristicAStar extends ConstraintHeuristic {
-    private ConstraintState initialState;
+	public ConstraintHeuristicAStar() {
+		super();
+	}
 
-    public ConstraintHeuristicAStar(ConstraintState initialState) {
-        super();
-        this.initialState = initialState;
-    }
+	@Override
+	public int f(ConstraintState s) {
+		return s.g() + this.h(s);
+	}
 
-    @Override
-    public int f(ConstraintState s) {
-        return s.g() + this.h(s);
-    }
+	@Override
+	public String toString() {
+		return "A* evaluation";
+	}
 
-    @Override
-    public int h(ConstraintState s) {
-        int combinedHeuristic = Integer.MAX_VALUE;
+	@Override
+	public int compare(ConstraintState s1, ConstraintState s2) {
+		int f1 = this.f(s1);
+		int f2 = this.f(s2);
 
-        if (s.goalIndex == null) {
-            // System.err.println("goal index is null");
-            Distances d = new ManhattanDistance(s.agentRows, s.agentCols, s.goals, s.boxes);
-            return d.calculate();
-        }
-        // Find the closest box to the goal state
-        int closestBoxHeuristic = Integer.MAX_VALUE;
-        for (int[] goalIndex : s.goalIndex) {
-            int goalRow = goalIndex[0];
-            int goalCol = goalIndex[1];
-            int closestBoxDistance = Integer.MAX_VALUE;
-
-            // If there are boxes
-            if (s.boxes.length > 0) {
-                for (int i = 0; i < s.boxes.length; i++) {
-                    int boxRow = s.boxes[i][0];
-                    int boxCol = s.boxes[i][1];
-                    int boxToGoalDistance = Math.abs(boxRow - goalRow) + Math.abs(boxCol - goalCol);
-                    closestBoxDistance = Math.min(closestBoxDistance, boxToGoalDistance);
-                }
-            }
-            // If there are no boxes, calculate the distance between the agent and the goal
-            else {
-                int agentRow = s.agentRows[s.agent];
-                int agentCol = s.agentCols[s.agent];
-                closestBoxDistance = Math.abs(agentRow - goalRow) + Math.abs(agentCol - goalCol);
-            }
-            closestBoxHeuristic = Math.min(closestBoxHeuristic, closestBoxDistance);
-        }
-
-        // Apply a penalty based on the number of constraints
-        int constraintPenalty = calculateConstraintPenalty(s);
-        combinedHeuristic = closestBoxHeuristic + constraintPenalty;
-
-        return combinedHeuristic;
-    }
-
-    @Override
-    public String toString() {
-        return "A* evaluation";
-    }
-
-    @Override
-    public int compare(ConstraintState s1, ConstraintState s2) {
-        int f1 = this.f(s1);
-        int f2 = this.f(s2);
-
-        if (f1 < f2) {
-            return -1;
-        } else if (f1 > f2) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
+		if (f1 < f2) {
+			return -1;
+		} else if (f1 > f2) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
 }
 
 class ConstraintHeuristicWeightedAStar extends ConstraintHeuristic {
-    private int w;
+	private int w;
 
-    public ConstraintHeuristicWeightedAStar(ConstraintState initialState, int w) {
-        super();
-        this.w = w;
-    }
+	public ConstraintHeuristicWeightedAStar(ConstraintState initialState, int w) {
+		super();
+		this.w = w;
+	}
 
-    @Override
-    public int f(ConstraintState s) {
-        return s.g() + this.w * this.h(s);
-    }
+	@Override
+	public int f(ConstraintState s) {
+		return s.g() + this.w * this.h(s);
+	}
 
-    @Override
-    public String toString() {
-        return String.format("WA*(%d) evaluation", this.w);
-    }
+	@Override
+	public String toString() {
+		return String.format("WA*(%d) evaluation", this.w);
+	}
 }
 
 class ConstraintHeuristicGreedy extends ConstraintHeuristic {
-    public ConstraintHeuristicGreedy(ConstraintState initialState) {
-        super();
-    }
+	public ConstraintHeuristicGreedy(ConstraintState initialState) {
+		super();
+	}
 
-    @Override
-    public int f(ConstraintState s) {
-        return this.h(s);
-    }
+	@Override
+	public int f(ConstraintState s) {
+		return this.h(s);
+	}
 
-    @Override
-    public String toString() {
-        return "greedy evaluation";
-    }
+	@Override
+	public String toString() {
+		return "greedy evaluation";
+	}
 }
-
